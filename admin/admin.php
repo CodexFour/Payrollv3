4da41ec24e6d958f1c56a1825d68b1fb6d0e35d4
MNizamD <ndatumanong05@gmail.com>
2023-12-17 01:18:33 +0800

    removed alert

--- admin/admin.php
+++ admin/admin.php
@@ -205,7 +205,6 @@
     }
     function clearEmployeeField(){
         let inputs = document.querySelectorAll('#add-employee-form input');
-        alert(inputs.length);
         inputs.forEach((i) =>{
             i.value = null;
         })
