c47f846dcd2c72247ba83fb30773c50128fd000d
MNizamD <ndatumanong05@gmail.com>
2023-12-18 21:45:39 +0800

    refix

--- employeeUi/setting.php
+++ employeeUi/setting.php
@@ -80,18 +80,30 @@
     <dialog id="privacy-policy" class="uni-card modal">
         <div class="flex center flex-row space-b">
             <h2>Privacy Policy</h2>
-            <button class="button-gray" onclick="showPassForm(false,'#privacy-policy')"><img src="assets/close.svg" alt=""></button>
+            <button class="button-gray" onclick="showPassForm(false,'#privacy-policy')">
+                <img src="assets/close.svg" style="margin-top: -10px; margin-right: -10px;">
+            </button>
         </div>
         <br>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione aut minima error distinctio quod amet voluptas sit, numquam eligendi explicabo vel exercitationem necessitatibus molestiae quaerat. Aliquid veniam nemo quod quibusdam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti id, numquam quae voluptate, sed et totam quidem aliquam ipsum nesciunt minus adipisci explicabo eveniet harum, in dignissimos rem itaque a.
+            Dicta quas eligendi rem. Molestias obcaecati laudantium facilis reiciendis praesentium quae quis necessitatibus optio aperiam asperiores. Id dicta enim quis consequuntur perspiciatis iusto ullam odio repellat nobis veniam, quod fugiat?
+            Vel deleniti adipisci officia possimus quos provident, itaque iusto reprehenderit, doloremque, omnis fugit veniam sunt eveniet. Natus voluptatem tempora amet consectetur inventore perspiciatis, a doloremque dignissimos, magni tempore aliquid aspernatur.
+            Fugiat deserunt perspiciatis ratione provident molestiae deleniti, veniam aliquam. Ratione iusto rem reiciendis doloremque mollitia neque pariatur officiis! Qui repudiandae velit dolore quaerat, consequuntur corrupti tempore deleniti rerum quisquam similique.
+            Quo, iusto et est, commodi autem molestiae modi dolorum ratione nesciunt sequi aliquam officiis! Eligendi illum harum et, quod repellendus cum. Ipsum distinctio eligendi, repudiandae rerum atque facilis quasi praesentium!</p>
     </dialog>
     <dialog id="terms-of-services" class="uni-card modal">
         <div class="flex center flex-row space-b">
             <h2>Terms of Services</h2>
-            <button class="button-gray" onclick="showPassForm(false,'#terms-of-services')"><img src="assets/close.svg" alt=""></button>
+            <button class="button-gray" onclick="showPassForm(false,'#terms-of-services')">
+                <img src="assets/close.svg" style="margin-top: -10px; margin-right: -10px;">
+            </button>
         </div>
         <br>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione aut minima error distinctio quod amet voluptas sit, numquam eligendi explicabo vel exercitationem necessitatibus molestiae quaerat. Aliquid veniam nemo quod quibusdam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam assumenda consectetur voluptates ipsum, facilis qui ab iusto vitae saepe distinctio, voluptas incidunt quo sequi nostrum sed iure est alias dolore!
+            Nostrum odio reiciendis magni tenetur quaerat magnam doloremque sit voluptas! Nostrum maiores ut consequatur nobis ipsam repellat placeat, ipsum at! Incidunt ipsum officia ex quas delectus ab deserunt, eum eaque.
+            Officiis a iste pariatur deleniti magnam enim molestiae perspiciatis accusamus nesciunt? Ullam, eaque impedit in maiores, accusantium laudantium atque repellat iusto temporibus ea nobis reprehenderit debitis libero fugiat beatae ipsum!
+            Neque, provident at? Blanditiis nulla reiciendis fugit autem consequatur mollitia sunt iusto eaque dolore cum qui dolores in dignissimos, est quaerat asperiores temporibus ea animi, pariatur aliquam ab optio? Saepe?
+            Dolorum similique rem obcaecati ad. Dignissimos quo laudantium quis. Delectus mollitia possimus, commodi voluptates sint asperiores a repellendus, optio, sed cumque aut? Dolor in cum, aliquam provident dolore perferendis rerum.</p>
     </dialog>
     <script>
         function showPassForm(bol, query) {
